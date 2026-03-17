6de351b7d8de097fba3d98479a4c2cc2047b42c5
nico359 <ambasingh@duck.com>
2026-03-17 09:55:35 +0100

    initial commit
    gnome builder app template

---
+++ README.md
@@ -0,0 +1,3 @@
+# expenses-rs
+
+A description of this project.
---
+++ src/window.rs
@@ -0,0 +1,69 @@
+/* window.rs
+ *
+ * Copyright 2026 Unknown
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+use gtk::prelude::*;
+use adw::subclass::prelude::*;
+use gtk::{gio, glib};
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default, gtk::CompositeTemplate)]
+    #[template(resource = "/io/github/nico359/expenses/window.ui")]
+    pub struct ExpensesRsWindow {
+        // Template widgets
+        #[template_child]
+        pub label: TemplateChild<gtk::Label>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for ExpensesRsWindow {
+        const NAME: &'static str = "ExpensesRsWindow";
+        type Type = super::ExpensesRsWindow;
+        type ParentType = adw::ApplicationWindow;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for ExpensesRsWindow {}
+    impl WidgetImpl for ExpensesRsWindow {}
+    impl WindowImpl for ExpensesRsWindow {}
+    impl ApplicationWindowImpl for ExpensesRsWindow {}
+    impl AdwApplicationWindowImpl for ExpensesRsWindow {}
+}
+
+glib::wrapper! {
+    pub struct ExpensesRsWindow(ObjectSubclass<imp::ExpensesRsWindow>)
+        @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow, adw::ApplicationWindow,        @implements gio::ActionGroup, gio::ActionMap;
+}
+
+impl ExpensesRsWindow {
+    pub fn new<P: IsA<gtk::Application>>(application: &P) -> Self {
+        glib::Object::builder()
+            .property("application", application)
+            .build()
+    }
+}
